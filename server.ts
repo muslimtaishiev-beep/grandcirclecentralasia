b3276713545755f345b2d8b12cd71eeb8317088e
Muslim Taishiev <muslimtaishiev@MacBook-Air-Muslim.local>
2026-07-11 01:49:17 +0600

    chore: fix TS error for PORT

--- server.ts
+++ server.ts
@@ -31,7 +31,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '50mb' }));
 
-const PORT = process.env.PORT || 3005;
+const PORT = Number(process.env.PORT) || 3005;
 const DB_PATH = path.join(process.cwd(), "data", "db.json");
 
 // Attempt to initialize Firebase Admin
